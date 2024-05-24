245b16738716aaac0292e8257f8ee8d728094fce
wcson <itnj.wcson@gmail.com>
2024-05-24 17:34:48 +0900

    OptionsApi vs ComponetApi

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
 	<div>
-		<button>Counter :{{ counter }}</button>
+		<button v-on:click="increment">Counter :{{ counter }}</button>
 	</div>
 </template>
 
 <script>
+import { onMounted, ref } from 'vue';
+
 export default {
-	data() {
+	setup() {
+		// eslint-disable-next-line no-undef
+		const counter = ref(0);
+		const increment = () => counter.value++;
+
+		onMounted(() => {
+			console.log('마운트 출력완료');
+		});
+
 		return {
-			counter: 0,
+			counter,
+			increment,
 		};
 	},
 };
