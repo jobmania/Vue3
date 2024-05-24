895163a4bc6059a18cd9ef2c961cd27af18fb020
wcson <itnj.wcson@gmail.com>
2024-05-24 15:01:48 +0900

    Vue 3 start

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+	<div>안녕</div>
+</template>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
+
+// 콘솔 사용금지 테스트
+console.log('hello nigro');
